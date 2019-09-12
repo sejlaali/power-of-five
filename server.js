ada7650f1ca3ad2d89fba2ce757637b71d1d0fa3
Sejla <44553582+sejlaali@users.noreply.github.com>
2019-09-12 12:17:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const app = express();
-const PORT = process.env.PORT || 3001;
-const cors = require('cors')
-
-app.use(logger("dev"));
-app.use(bodyParser.json());
-
-app.use((err, req, res, next) => {
-  console.warn(err.stack);
-  res.status(500).json({ msg: err.message });
-});
-app.use(cors())
-
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
-=======
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -37,4 +15,3 @@ app.use('/users', usersRouter);
 app.use('/moods', moodsRouter);
 
 app.listen(3000, () => console.log('up and running'));
->>>>>>> Stashed changes
